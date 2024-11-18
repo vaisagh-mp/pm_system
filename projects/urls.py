f9f06ab8194f0b08042a680ab83a6b34559a18d0
VAISAGH MP <122623129+vaisagh-mp@users.noreply.github.com>
2024-11-18 17:14:50 +0530

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProjectListCreateView, ProjectDetailView
 
 urlpatterns = [
-    path(' ', ProjectListCreateView.as_view(), name='project-list-create'),
+    path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
 ]
