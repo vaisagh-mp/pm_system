c5bd5e5d07caf4170969cc300aae41a149928c08
VAISAGH MP <122623129+vaisagh-mp@users.noreply.github.com>
2024-11-18 17:04:54 +0530

    Update urls.py

--- pm_system/urls.py
+++ pm_system/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-    path('api/', include('projects.urls')),
+    path(' ', include('projects.urls')),
     path('api/', include('tasks.urls')),
     path('api/notifications/', include('notifications.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
